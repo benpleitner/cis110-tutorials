f80a7b0f46b798007de5f2bdb84e2fa854de6ed3
Ben <benpleitner@gmail.com>
2015-08-10 20:19:53 -0400

    Added button function calls and improved timing

--- static/js/f14_5.1.js
+++ static/js/f14_5.1.js
@@ -1,57 +1,67 @@
-answerTheater();
-teacherTheater();
+$("#nextStep1").css("display", "none")
 
-function answerTheater() {
+function clickWalkthroughButton() {
+	var delay= 21000; //miliseconds
+
+	setTimeout(function(){
+		$("#nextStep1").css("display", "inherit")
+	}, delay); 
+
+	$("#walkthroughButton").css("display", "none")
 	var theater = new TheaterJS();
+	var theater1 = new TheaterJS();
 
-	theater
-	 .describe("Line_1", { speed: 1, accuracy: 1 }, "#line1")
-	 .describe("Line_2", { speed: 1, accuracy: 1 }, "#line2")
+	answerTheater(theater);
+	teacherTheater(theater1);
 
-	theater
-	  .on("say:start, erase:start", function (eventName) {
-	    var self = this,
-	        current = self.current.voice;
+	function answerTheater(theater) {
+		theater
+		 .describe("Line_1", { speed: 1, accuracy: 1 }, "#line1")
+		 .describe("Line_2", { speed: 1, accuracy: 1 }, "#line2")
 
-	    self.utils.addClass(current, "saying");
-	  })
-	  .on("say:end, erase:end", function (eventName) {
-	    var self = this,
-	        current = self.current.voice;
+		theater
+		  .on("say:start, erase:start", function (eventName) {
+		    var self = this,
+		        current = self.current.voice;
 
-	    self.utils.removeClass(current, "saying");
-	  });
+		    self.utils.addClass(current, "saying");
+		  })
+		  .on("say:end, erase:end", function (eventName) {
+		    var self = this,
+		        current = self.current.voice;
 
-	theater
-		.write(8200)
-	  .write("Line_1: public static boolean sameSign", 9200, "(int[] arr) {", 1)
-	  .write("Line_2: }", 1600)
-	  .write(function () { theater.play(false); });
-}
+		    self.utils.removeClass(current, "saying");
+		  });
 
-function teacherTheater() {
-	var theater = new TheaterJS();
+		theater
+			.write(8200)
+		  .write("Line_1: public static boolean sameSign", 9200, "(int[] arr) {", 1)
+		  .write("Line_2: }", 1600)
+		  .write(function () { theater.play(false); });
+	}
 
-	theater
-	 .describe("Line_1", { speed: 1, accuracy: 1 }, "#line1T")
-	 .describe("Line_2", { speed: 1, accuracy: 1 }, "#line2T")
+	function teacherTheater(theater) {
+		theater
+		 .describe("Line_1", { speed: 1, accuracy: 1 }, "#line1T")
+		 .describe("Line_2", { speed: 1, accuracy: 1 }, "#line2T")
 
-	theater
-	  .on("say:start, erase:start", function (eventName) {
-	    var self = this,
-	        current = self.current.voice;
+		theater
+		  .on("say:start, erase:start", function (eventName) {
+		    var self = this,
+		        current = self.current.voice;
 
-	    self.utils.addClass(current, "saying");
-	  })
-	  .on("say:end, erase:end", function (eventName) {
-	    var self = this,
-	        current = self.current.voice;
+		    self.utils.addClass(current, "saying");
+		  })
+		  .on("say:end, erase:end", function (eventName) {
+		    var self = this,
+		        current = self.current.voice;
 
-	    self.utils.removeClass(current, "saying");
-	  });
+		    self.utils.removeClass(current, "saying");
+		  });
 
-	theater
-	  .write("Line_1: Since the question specifies to return true under a certain circumstance, and false otherwise, we know that sameSign() has a boolean return type.", 2400)
-	  .write("Line_2: The question also tells us that sameSign() 'takes an array of integers,' so we know that our function has one argument, which is an array of integers. Let's call it 'arr.'", 1600)
-	  .write(function () { theater.play(false); });
+		theater
+		  .write("Line_1: Since the question specifies to return true under a certain circumstance, and false otherwise, we know that sameSign() has a boolean return type.", 2400)
+		  .write("Line_2: The question also tells us that sameSign() 'takes an array of integers,' so we know that our function has one argument, which is an array of integers. Let's call it 'arr.'", 1600)
+		  .write(function () { theater.play(false); });
+	}
 }
