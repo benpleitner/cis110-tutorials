36ac940fecf0cd8156bd2852a7b84778460ee543
Ben <benpleitner@gmail.com>
2015-08-10 20:04:52 -0400

    Added first step of fall 2014 5.1

---
+++ static/js/f14_5.1.js
@@ -0,0 +1,57 @@
+answerTheater();
+teacherTheater();
+
+function answerTheater() {
+	var theater = new TheaterJS();
+
+	theater
+	 .describe("Line_1", { speed: 1, accuracy: 1 }, "#line1")
+	 .describe("Line_2", { speed: 1, accuracy: 1 }, "#line2")
+
+	theater
+	  .on("say:start, erase:start", function (eventName) {
+	    var self = this,
+	        current = self.current.voice;
+
+	    self.utils.addClass(current, "saying");
+	  })
+	  .on("say:end, erase:end", function (eventName) {
+	    var self = this,
+	        current = self.current.voice;
+
+	    self.utils.removeClass(current, "saying");
+	  });
+
+	theater
+		.write(8200)
+	  .write("Line_1: public static boolean sameSign", 9200, "(int[] arr) {", 1)
+	  .write("Line_2: }", 1600)
+	  .write(function () { theater.play(false); });
+}
+
+function teacherTheater() {
+	var theater = new TheaterJS();
+
+	theater
+	 .describe("Line_1", { speed: 1, accuracy: 1 }, "#line1T")
+	 .describe("Line_2", { speed: 1, accuracy: 1 }, "#line2T")
+
+	theater
+	  .on("say:start, erase:start", function (eventName) {
+	    var self = this,
+	        current = self.current.voice;
+
+	    self.utils.addClass(current, "saying");
+	  })
+	  .on("say:end, erase:end", function (eventName) {
+	    var self = this,
+	        current = self.current.voice;
+
+	    self.utils.removeClass(current, "saying");
+	  });
+
+	theater
+	  .write("Line_1: Since the question specifies to return true under a certain circumstance, and false otherwise, we know that sameSign() has a boolean return type.", 2400)
+	  .write("Line_2: The question also tells us that sameSign() 'takes an array of integers,' so we know that our function has one argument, which is an array of integers. Let's call it 'arr.'", 1600)
+	  .write(function () { theater.play(false); });
+}
